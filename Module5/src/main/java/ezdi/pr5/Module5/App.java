b34e6a652ab0dcb6a48791e5085bea92d4065600
Rajesh Dhamsaniya <EZDI\rajesh.d@rajeshd.ezdi.local>
2017-05-23 11:08:59 +0530

    update

---
+++ Module5/src/main/java/ezdi/pr5/Module5/App.java
@@ -0,0 +1,13 @@
+package ezdi.pr5.Module5;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
