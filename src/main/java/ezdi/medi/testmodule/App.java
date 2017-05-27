39d5dd5c7eb5899bb3bc7ab892747f26fc0a1479
rajeshd-ezdi <rajesh.d@ezdi.us>
2017-05-27 12:03:08 +0530

    hello

---
+++ src/main/java/ezdi/medi/testmodule/App.java
@@ -0,0 +1,13 @@
+package ezdi.medi.testmodule;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "this is new project" );
+    }
+}
